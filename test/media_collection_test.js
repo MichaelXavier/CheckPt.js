6b18fc688920148de3de5c91d8533314617330f7
Michael Xavier <michael@michaelxavier.net>
2010-12-20 20:59:00 -0800

    not all tests run, but i think they all pass

--- test/media_collection_test.js
+++ test/media_collection_test.js
@@ -1,81 +1,119 @@
 var testCase        = require('nodeunit').testCase,
     MediaCollection = require('../lib/media_collection');
 
+function buildCollection(cb) {
+  this.collection = new MediaCollection({'name': "Curb Your Enthusiasm"});
+  cb();
+}
+
+function deleteCollection(cb) {
+  this.collection = null;
+  cb();
+}
+
+//TODO: dry up setup
 module.exports = {
   'constructor': testCase({
+    setUp: function(cb) {
+      this.collection = new MediaCollection({'name': "Curb Your Enthusiasm"});
+      cb();
+    },
+
     'sets name': function(t) {
       t.expect(1);
-      t.equals("Curb Your Enthusiasm", MediaCollection("Curb Your Enthusiasm").name);
+      t.equals("Curb Your Enthusiasm", this.collection.get('name'));
       t.done();
     },
+
     'defaults empty items': function(t) {
       t.expect(1);
-      t.equals(0, MediaCollection("x").items.length);
+      t.equals(0, this.collection.get('items').length);
       t.done();
     },
-    'progress': testCase({
-      setUp: function(cb) {
-        this.collection = MediaCollection("x");
-        cb();
-      },
-      'is 0.0 on empty list': function(t) {
-        t.expect(1);
-        t.equals(0.0, this.collection.progress());
-        t.done();
-      },
-      'returns the ratio of completed to incomplete': function(t) {
-        t.expect(1);
-        //Push some mock MediaItems
-        this.collection.items.push({completed:false});
-        this.collection.items.push({completed:true});
-        this.collection.items.push({completed:true});
-        t.equals(2/3, this.collection.progress());
-        t.done();
-      },
-    }),
-    'remaining_list': testCase({
-      setUp: function(cb) {
-        this.collection = MediaCollection("x");
-        cb();
-      },
-      'returns empty list on empty collection': function(t) {
-        t.expect(1);
-        t.equals(0, this.collection.remaining_list().length);
-        t.done();
-      },
-      'returns only the incomplete items': function(t) {
-        t.expect(2);
-        //Push some mock MediaItems
-        this.collection.items.push({completed:false});
-        this.collection.items.push({completed:true});
-        this.collection.items.push({completed:true});
-        var remaining = this.collection.remaining_list();
-        t.equals(1, remaining.length);
-        t.equals(false, remaining[0].completed);
-        t.done();
-      }
-    }),
-    'completed_list': testCase({
-      setUp: function(cb) {
-        this.collection = MediaCollection("x");
-        cb();
-      },
-      'returns empty list on empty collection': function(t) {
-        t.expect(1);
-        t.equals(0, this.collection.completed_list().length);
-        t.done();
-      },
-      'returns only the incomplete items': function(t) {
-        t.expect(2);
-        //Push some mock MediaItems
-        this.collection.items.push({completed:false});
-        this.collection.items.push({completed:true});
-        this.collection.items.push({completed:true});
-        var completed = this.collection.completed_list();
-        t.equals(2, completed.length);
-        t.equals(true, completed.every(function(i) { return i.completed; }));
-        t.done();
-      }
-    }),
+
+    tearDown: function(cb) {
+      this.collection = null;
+      cb();
+    }
+  }),
+  'progress': testCase({
+    setUp: function(cb) {
+      this.collection = new MediaCollection({'name': "Curb Your Enthusiasm"});
+      cb();
+    },
+
+    'is 0.0 on empty list': function(t) {
+      t.expect(1);
+      t.equals(0.0, this.collection.progress());
+      t.done();
+    },
+
+    'returns the ratio of completed to incomplete': function(t) {
+      t.expect(1);
+      //Push some mock MediaItems
+      this.collection.push({completed:false});
+      this.collection.push({completed:true});
+      this.collection.push({completed:true});
+      t.equals(2/3, this.collection.progress());
+      t.done();
+    },
+
+    tearDown: function(cb) {
+      this.collection = null;
+      cb();
+    }
+  }),
+
+  'remaining_list': testCase({
+    setUp: function(cb) {
+      this.collection = new MediaCollection({'name': "Curb Your Enthusiasm"});
+      cb();
+    },
+
+    'returns empty list on empty collection': function(t) {
+      t.expect(1);
+      t.equals(0, this.collection.remaining_list().length);
+      t.done();
+    },
+
+    'returns only the incomplete items': function(t) {
+      t.expect(2);
+      //Push some mock MediaItems
+      this.collection.push({completed:false});
+      this.collection.push({completed:true});
+      this.collection.push({completed:true});
+      var remaining = this.collection.remaining_list();
+      t.equals(1, remaining.length);
+      t.equals(false, remaining[0].completed);
+      t.done();
+    },
+
+    tearDown: function(cb) {
+      this.collection = null;
+      cb();
+    }
+  }),
+
+  'completed_list': testCase({
+    setUp: function(cb) {
+      this.collection = new MediaCollection({'name': "Curb Your Enthusiasm"});
+      cb();
+    },
+    'returns empty list on empty collection': function(t) {
+      t.expect(1);
+      t.equals(0, this.collection.completed_list().length);
+      t.done();
+    },
+    'returns only the incomplete items': function(t) {
+      t.expect(2);
+      //Push some mock MediaItems
+      this.collection.push({completed:false});
+      this.collection.push({completed:true});
+      this.collection.push({completed:true});
+      var completed = this.collection.completed_list();
+      t.equals(2, completed.length);
+      t.equals(true, completed.every(function(i) { return i.completed; }));
+      t.done();
+    }
   })
 };
--- test/media_item_test.js
+++ test/media_item_test.js
@@ -3,57 +3,67 @@ var testCase  = require('nodeunit').testCase,
 
 module.exports = {
   'constructor': testCase({
+    setUp: function(cb) {
+      this.item = new MediaItem({'name': 'Episode 1'});
+    },
+
     'sets name': function(t) {
       t.expect(1);
-      t.equals("Episode 1", MediaItem("Episode 1").name);
+      t.equals("Episode 1", this.item.get('name'));
       t.done();
     },
     'defaults to incomplete': function(t) {
       t.expect(1);
-      t.equals(false, MediaItem("x").completed);
+      t.equals(false, this.item.get('completed'));
       t.done();
     },
     'defaults compled_on to null': function(t) {
       t.expect(1);
-      t.equals(null, MediaItem("x").completed_on);
+      t.equals(null, this.item.get('completed_on'));
       t.done();
     },
   }),
+
   'complete': testCase({
     setUp: function(cb) {
-      this.item = MediaItem("x");
+      this.item = new MediaItem({'name': 'Episode 1'});
       cb();
     },
+
     'marks as complete': function(t) {
       t.expect(1);
       this.item.complete();
-      t.equals(true, this.item.completed);
+      t.equals(true, this.item.get('completed'));
       t.done();
     },
+
     'marks the time': function(t) {
       t.expect(1);
       var when = new Date('Dec 19, 2010');
       this.item.complete(when);
-      t.equals(when, this.item.completed_on);
+      t.equals(when, this.item.get('completed_on'));
       t.done();
     }
   }),
+
   'incomplete': testCase({
     setUp: function(cb) {
-      this.item = MediaItem("x");
+      this.item = new MediaItem({'name': 'Episode 1'});
       this.item.complete(new Date());
       cb();
     },
+
     'marks as incomplete': function(t) {
       t.expect(1);
       this.item.incomplete();
-      t.equals(false, this.item.completed);
+      t.equals(false, this.item.get('completed'));
       t.done();
     },
+
     'clears the time': function(t) {
       t.expect(1);
       this.item.incomplete();
-      t.equals(null, this.item.completed_on);
+      t.equals(null, this.item.get('completed_on'));
       t.done();
     }
   })
