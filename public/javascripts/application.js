e69f55a19890afb8395a812d31e96bb669a04e7c
Michael Xavier <xavier@crystalcommerce.com>
2010-12-23 13:20:37 -0800

    Toying with progress bars

---
+++ public/javascripts/application.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  $('#progress_bar').
+  progressbar({
+    value: 37,
+    complete: function(event, ui) {
+      $(this).children('.ui-progressbar-value').
+              removeClass('collection_progress_incomplete').
+              addClass('collection_progress_complete');
+    },
+  }).
+  children('.ui-progressbar-value').
+  addClass('collection_progress_incomplete');
+});
