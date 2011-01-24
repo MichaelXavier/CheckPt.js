f76052a178fdac530428ff715e2b80b9c4af4601
Michael Xavier <michael@michaelxavier.net>
2011-01-23 17:53:38 -0800

    Fix bug on MC create
    When the view was created it would not use the response to set the key.

--- public/javascripts/views/new_media_collection_view.js
+++ public/javascripts/views/new_media_collection_view.js
@@ -5,13 +5,15 @@ window.NewMediaCollectionView = Backbone.View.extend({
     //prepend before where the form goes
     $('#new_media_collection').before(this.el);
 
-    var rendered_form = $(this.form_template());
+    var rendered_form = $(this.form_template()),
+        callback      = this.response_callback;
     this.el.click(function() {
-      //FIXME: this is refreshing the page. STOP IT
       rendered_form.submit(function(event) {
         // Add a media collection to the app, which should save
-        var mc = App.collection.create(serializeForm($(event.target)));
-        new MediaCollectionView({model: mc}).render($('#app'));
+        var mc = App.collection.create(serializeForm($(event.target)), {
+          error: callback,
+          success: callback
+        });
         $(this).remove(); //The form is no longer needed
         return false;
       });
@@ -21,5 +23,17 @@ window.NewMediaCollectionView = Backbone.View.extend({
   },
 
   template: _.template($('#new_media_collection_template').html()),
-  form_template: _.template($('#new_media_collection_form_template').html())
+  form_template: _.template($('#new_media_collection_form_template').html()),
+
+  //FIXME: this is wrong but sync is coming back as erroneous
+  response_callback: function(model, response) {
+    if (response.status == 200) {
+      //Response body will be the DB key
+      //FIXME: for some reason if I don't do both, the record is still
+      //considered new
+      model.attributes.id = response.responseText;
+      model.id = response.responseText;
+      new MediaCollectionView({model: model}).render($('#app'));
+    }
+  }
 });
